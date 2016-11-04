dc563fe3d5df933d6791e989679d952329d2a188
noby <n.fujioka@abacusmaths.info>
2016-11-04 21:39:43 +0000

    inserts new objects into the database

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -9,15 +9,31 @@
 namespace AppBundle\Controller;
 
 
+
+use AppBundle\Entity\Genus;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
-
+use Symfony\Component\HttpFoundation\Response;
 
 
 class GenusController extends Controller
 {
+    /**
+     * @Route("/genus/new")
+     */
+    public function newAction()
+    {
+        $genus = new Genus();
+        $genus->setName('Octopus'.rand(1, 100));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($genus);
+        $em->flush();
+
+        return new Response('<html><body>Genus created!</body></html>');
+    }
 
     /**
      * @Route("/genus/{genusName}")
--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -27,4 +27,22 @@ class Genus
      * @ORM\Column(type="string")
      */
     private $name;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+
 }
