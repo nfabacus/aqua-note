0c76af5b066fcdb42ce0792da7f1320c85fb48f3
noby <n.fujioka@abacusmaths.info>
2016-11-04 20:17:54 +0000

    adds Genus class for Doctrine ORM

---
+++ src/AppBundle/Entity/Genus.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nobuyukifujioka
+ * Date: 04/11/2016
+ * Time: 20:10
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="genus")
+ */
+class Genus
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+}
